a4364d4c1e666a4198a3f931a029d7562c41ff4e
Joshua Kim <joshuakim135@gmail.com>
2021-10-05 19:08:42 -0400

    Skeleton Code

--- shell.cpp
+++ shell.cpp
@@ -1,3 +1,32 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
 
 using namespace std;
+
+int main() {
+
+    // int in_def = dup(0);
+    // int out_def = dup(1);
+
+    while (true) {
+        // dup2(in_def, 0);
+        // dup2(out_def, 1);
+
+        cout << "My Shell$ ";
+        string inputline;
+        getline (cin, inputline);
+        if (inputline == string("exit")) {
+            cout << "Bye!! End of shell" << endl;
+            break;
+        }
+        int pid = fork();
+        if(pid == 0) {
+            char* args [] = {(char *) inputline.c_str(), NULL};
+            execvp (args [0], args);
+        }
+    }
+
+
+    return 0;
+}
